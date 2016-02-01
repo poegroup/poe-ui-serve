6ad4b55c8480a42fa4e58c82b49a0038f7bd7487
camshaft <bytheway.cameron@gmail.com>
2016-01-31 22:12:38 -0700

    set buildPath and manifest options

--- app.js
+++ app.js
@@ -10,13 +10,21 @@ var resolve = require('path').resolve;
  * Expose the app
  */
 
-var app = module.exports = stack({
-  root: __dirname,
-});
 
-app.builder.resolve.modulesDirectories.push(__dirname + '/node_modules');
+var opts = {
+  root: __dirname
+};
 
 if (process.env.POE_BUILD_PATH) app.builder.output.path = process.env.POE_BUILD_PATH;
+
+if (process.env.NODE_ENV == 'production') {
+  opts.buildPath = process.cwd() + '/build';
+  opts.manifest = process.cwd() + '/manifest.json';
+}
+
+var app = module.exports = stack(opts);
+
+app.builder.resolve.modulesDirectories.push(__dirname + '/node_modules');
 
 app.builder.addES6({
   test: /.(js)$/,
