90ff1afbd84d1217f828e6c6b6d34a5103c20e5e
camshaft <bytheway.cameron@gmail.com>
2016-01-31 22:16:08 -0700

    let's get the order correct

--- app.js
+++ app.js
@@ -6,24 +6,22 @@ var stack = require('poe-ui/server');
 var dirname = require('path').dirname;
 var resolve = require('path').resolve;
 
-/**
- * Expose the app
- */
-
-
 var opts = {
   root: __dirname
 };
-
-if (process.env.POE_BUILD_PATH) app.builder.output.path = process.env.POE_BUILD_PATH;
 
 if (process.env.NODE_ENV == 'production') {
   opts.buildPath = process.cwd() + '/build';
   opts.manifest = process.cwd() + '/manifest.json';
 }
 
+/**
+ * Expose the app
+ */
+
 var app = module.exports = stack(opts);
 
+if (process.env.POE_BUILD_PATH) app.builder.output.path = process.env.POE_BUILD_PATH;
 app.builder.resolve.modulesDirectories.push(__dirname + '/node_modules');
 
 app.builder.addES6({
